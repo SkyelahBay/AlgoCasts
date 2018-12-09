ed02d6db128c0e6920cfc66c3a6fe37f75f5a626
Skyelah <skyelahbay@gmail.com>
2018-12-09 14:08:31 -0600

    added solution for maxChar that also checks for ties

--- exercises/maxchar/index.js
+++ exercises/maxchar/index.js
@@ -5,6 +5,37 @@
 // maxChar("abcccccccd") === "c"
 // maxChar("apple 1231111") === "1"
 
-function maxChar(str) {}
 
-module.exports = maxChar;
+// also catches cases where you have 2+ characters with the same max count.
+const maxChar = (str) => {
+
+  // initial setup of variables and changing letters to be the same case
+  let lowerString = str.toLowerCase(),
+      charCounts = {},
+      currentMax = 1,
+      maxChars = [];
+
+  // get a count of each character in the string.
+  for(let char of lowerString){
+    charCounts[char] = charCounts.hasOwnProperty(char) ? charCounts[char] + 1 : 1;
+  }
+
+  // enumerate over every character and find the most common ones.
+  for(let char in charCounts){
+    if(charCounts[char] === currentMax) maxChars.push(char) // if there is a tie
+
+    if(charCounts[char] > currentMax){
+      maxChars = [];                     // reset the array.
+      currentMax = charCounts[char];     // set the new max.
+      maxChars.push(char);               // push the character.
+    }
+  }
+
+  //return it :)
+  return maxChars
+}
+
+
+module.exports = {
+  maxChar
+}
--- exercises/maxchar/test.js
+++ exercises/maxchar/test.js
@@ -1,14 +1,20 @@
-const maxChar = require('./index');
+const maxCharMethods = require('./index');
 
-test('maxChar function exists', () => {
-  expect(typeof maxChar).toEqual('function');
-});
+describe('#maxChar function', () => {
 
-test('Finds the most frequently used char', () => {
-  expect(maxChar('a')).toEqual('a');
-  expect(maxChar('abcdefghijklmnaaaaa')).toEqual('a');
-});
+  test('maxChar function exists', () => {
+    expect(typeof maxCharMethods.maxChar).toEqual('function');
+  });
+  
+  test('Finds the most frequently used chars', () => {
+    expect(maxCharMethods.maxChar('a')).toEqual(['a']);
+    expect(maxCharMethods.maxChar('abcdefghijklmnaaaaa')).toEqual(['a']);
+    expect(maxCharMethods.maxChar('aeeaiou')).toEqual(['a', 'e']);
+    expect(maxCharMethods.maxChar('aeeaiouu')).toEqual(['a', 'e', 'u']);
+  });
+  
+  test('Works with numbers in the string', () => {
+    expect(maxCharMethods.maxChar('ab1c1d1e1f1g1')).toEqual(['1']);
+  });
 
-test('Works with numbers in the string', () => {
-  expect(maxChar('ab1c1d1e1f1g1')).toEqual('1');
 });
